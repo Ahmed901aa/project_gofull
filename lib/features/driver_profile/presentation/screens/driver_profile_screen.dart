05691eb9d89f6a1213b58cc4d53eef70a7560ffb
Ahmed901aa <ahmed.ly2025@gmail.com>
2026-04-06 14:13:46 +0200

    spaceing

--- lib/features/driver_profile/presentation/screens/driver_profile_screen.dart
+++ lib/features/driver_profile/presentation/screens/driver_profile_screen.dart
@@ -49,7 +49,8 @@ class DriverProfileScreen extends StatelessWidget {
                   ],
                 ),
               );
-                },
+              },
+              ),
             ),
           ],
         ),
